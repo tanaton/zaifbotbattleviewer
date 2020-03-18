33dcb22b68a5f9eff0698e440d07d9db1bfb8eee
tanaton <heiwaboke@gmail.com>
2020-03-18 13:33:03 +0900

    トチ狂って買い増しした

--- ts/myasset.ts
+++ ts/myasset.ts
@@ -74,8 +74,8 @@ const ticksUrl = "/api/zaif/1/ticks/xem_jpy";
 const floatFormat = d3.format(".1f");
 const PriceMax = 10_000_000;
 const PriceMin = -10_000_000;
-const CurrencyNumber = 181_886;
-const PurchasePrice = 2_300_000 / CurrencyNumber;
+const CurrencyNumber = 261_634;
+const PurchasePrice = 2_600_000 / CurrencyNumber;
 
 type Box = {
     readonly top: number;
